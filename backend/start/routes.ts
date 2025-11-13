e314b08a2f9f5f9d215c581e7d694725793eaff2
Perilix <166854906+Perilix@users.noreply.github.com>
2025-11-13 15:52:08 +0100

    chore: rollback patch user

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -41,7 +41,7 @@ router
         router.post('/', '#controllers/users_controller.create')
         router.get('/trashed', '#controllers/users_controller.trashed')
         router.get('/:id', '#controllers/users_controller.show')
-        router.put('/:id', '#controllers/users_controller.update')
+        router.patch('/:id', '#controllers/users_controller.update')
         router.delete('/:id', '#controllers/users_controller.delete').use(middleware.auth())
         router.post('/:id/restore', '#controllers/users_controller.restore')
       })
