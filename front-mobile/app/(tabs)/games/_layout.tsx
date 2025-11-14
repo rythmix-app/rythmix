d38be055e002daf3903463dc34a825d8913cc721
Ethanol <98337020+Ethanol55@users.noreply.github.com>
2025-11-14 10:15:03 +0100

    MIX-177 feat: create principal layout on front-mobile (#51)

---
+++ front-mobile/app/(tabs)/games/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+export default function GamesLayout() {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+                name="index"
+                options={{ title: 'Jeux' }}
+            />
+        </Stack>
+    );
+}
