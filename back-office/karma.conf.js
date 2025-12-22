e54b12bf22d2727eef9778df861e707dbb67b8df
Perilix <166854906+Perilix@users.noreply.github.com>
2025-12-22 15:18:29 +0100

    MIX-187 feat: add karm.conf for test

---
+++ back-office/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/back-office'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true,
+    singleRun: false
+  });
+};
