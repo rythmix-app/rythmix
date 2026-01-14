4a53c52dfc24c7a17248f95e13fbb2d420199411
Wu Qin-Hao <98337187+KyNaOo@users.noreply.github.com>
2026-01-14 13:16:48 +0100

    MIX-202 feat: adding password strenght indicator (#76)

--- front-mobile/app/auth/register.tsx
+++ front-mobile/app/auth/register.tsx
@@ -3,6 +3,7 @@ import Input from "@/components/Input";
 import { Link, router } from "expo-router";
 import { RythmixLogo } from "@/components/RythmixLogo";
 import { useState } from "react";
+import PasswordStrengthIndicator from "@/components/auth/PasswordStrengthIndicator";
 import {
   Alert,
   ScrollView,
@@ -168,6 +169,8 @@ export default function RegisterScreen() {
         labelStyle={styles.labelStyle}
         editable={!isLoading}
       />
+
+      <PasswordStrengthIndicator password={password} />
 
       {/* Confirm Password */}
       <Input
